503c840712f147cb2592bfca100b8c34c8592589
Lior <liorshor123@gmail.com>
2020-12-08 23:20:04 +0200

    Deleted findmybarber.findmybarber

--- app/src/main/java/com/findmybarber/findmybarber/controller/User.java
+++
@@ -1,9 +0,0 @@
-package com.findmybarber.findmybarber.controller;
-
-public interface User {
-    String getUserEmail();
-    String getUserName();
-    String getUserSurname();
-    String getUserPhoneNumber();
-    UserType getUserType();
-}
